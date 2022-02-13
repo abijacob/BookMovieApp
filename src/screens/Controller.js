6a56cff4ef628787f29bea2f61d65b2fa68012a8
abijacob <abijacob92@gmail.com>
2022-02-13 13:59:09 +0530

    Header and Login button added

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,9 +1,6 @@
 import React from "react";
-import Home from "../screens/home/Home";
-import Details from "../screens/details/Details";
+import Home from "../common/header/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import BookShow from "../screens/bookshow/BookShow";
-import Confirmation from "../screens/confirmation/Confirmation";
 
 const Controller = () => {
   const baseUrl = "/api/v1/";
@@ -16,18 +13,7 @@ const Controller = () => {
           path="/"
           render={(props) => <Home {...props} baseUrl={baseUrl} />}
         />
-        <Route
-          path="/movie/:id"
-          render={(props) => <Details {...props} baseUrl={baseUrl} />}
-        />
-        <Route
-          path="/bookshow/:id"
-          render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
-        />
-        <Route
-          path="/confirm/:id"
-          render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
-        />
+
       </div>
     </Router>
   );
